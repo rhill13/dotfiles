25fc59c11836879eaffd2f726056d412cbd998fc
Ryan Hill <ryanhill1128@gmail.com>
2024-11-22 14:52:48 -0600

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,57 @@
+{ config, pkgs, ... }: {
+  imports = [ ./hardware-configuration.nix ];
+
+  # Enable flakes and nix-command
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Boot loader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "temp";
+  networking.networkmanager.enable = true;
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    neovim
+    git
+    wget
+    curl
+    ly
+  ];
+
+  # User account
+  users.users.ryan = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    initialPassword = "changeme";
+  };
+
+  # System settings
+  time.timeZone = "UTC";
+  system.stateVersion = "24.05";
+
+  # Enable sound
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # X11 and Window Manager
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    libinput.enable = true;  # Enable touchpad support
+    
+    windowManager.bspwm.enable = true;
+    
+    # Disable default display manager
+    displayManager.lightdm.enable = false;
+  };
+
+  # Enable ly display manager
+  services.ly = {
+    enable = true;
+    animation = true;  # Enable matrix animation
+    defaultUser = "ryan";
+  };
+}
