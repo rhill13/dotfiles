e939d745ca5351f129e22662ec8bed9a894d2709
Ryan Hill <ryanhill1128@gmail.com>
2024-11-22 14:57:20 -0600

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "NixOS configuration for ThinkPad T14s";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org/"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations.thinkpad = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.ryan = import ./home.nix;
+          }
+        ];
+      };
+    };
+}
